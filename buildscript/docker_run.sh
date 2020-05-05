3eef7e308e3a12ccf4014e47d77734afe66454da
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-05-05 09:13:09 +0530

    Update docker_run.sh

--- buildscript/docker_run.sh
+++ buildscript/docker_run.sh
@@ -2,4 +2,4 @@ export ARCH="aarch64"
 cd $(dirname $0)
 srcdir=$( cd "$(dirname $0)/../.."  && pwd )
 echo "srcdir=$srcdir"
-docker run --rm -v $srcdir:/root/jobber alpine:latest /bin/sh /root/jobber/buildscripts/build_alpine.sh 
+docker run --rm -v $srcdir:/root/jobber alpine:latest /bin/sh /root/jobber/buildscript/build_alpine.sh 
